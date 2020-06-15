2e93cd0d251592266bdb472f73233f269b127041
Jehdi Aizon <46204336+jaizon2000@users.noreply.github.com>
2020-06-15 16:16:53 -0600

    Add files via upload

--- file.c
+++ file.c
@@ -10,10 +10,26 @@ int main() {
     exit(EXIT_FAILURE);
   }
 
-  char s[80];
+  char s[80]; // max 80 char in one word
+  int count = 0;
+
+  srand((unsigned) time(NULL));
+ 
   while (fgets(s, 80, f) != NULL) {
+
+    printf("\nftell: %ld\n", ftell(f));
     printf("%s", s);
+    count++;
   }
+  
+  long int rand_num = 100000 + (rand() % (count-100000));
+  printf("\ncount: %d | NUM: %ld\n", count,  rand_num);
+  printf("fseek: %d\n", fseek(f, rand_num, SEEK_SET));
+  fgets(s, 80, f);
+  printf("%s", s);
+  printf("\nftell: %ld\n", ftell(f));
+  
+
   
   fclose(f);
   
