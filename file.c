2d307cb31bae9b8a9aa157654ff784eb7c0995f9
Jehdi Aizon <46204336+jaizon2000@users.noreply.github.com>
2020-06-15 16:17:27 -0600

    Prints random word

--- file.c
+++ file.c
@@ -4,33 +4,36 @@
 
 int main() {
   FILE *f;
-    
+  
+  // OPEN FILE - Check for failure
   f = fopen("/usr/share/dict/words", "r");
   if (f == NULL) {
     exit(EXIT_FAILURE);
   }
-
-  char s[80]; // max 80 char in one word
+  
+  char word[80]; // max 80 char in 1 word
   int count = 0;
-
-  srand((unsigned) time(NULL));
+  srand((unsigned) time(NULL)); // randomizing  rand()
+  
  
-  while (fgets(s, 80, f) != NULL) {
-
-    printf("\nftell: %ld\n", ftell(f));
-    printf("%s", s);
+  while (fgets(word, 80, f) != NULL) {
+    /* printf("\nftell: %ld\n", ftell(f)); */
+    /* printf("%s", s); */
     count++;
   }
   
-  long int rand_num = 100000 + (rand() % (count-100000));
-  printf("\ncount: %d | NUM: %ld\n", count,  rand_num);
-  printf("fseek: %d\n", fseek(f, rand_num, SEEK_SET));
-  fgets(s, 80, f);
-  printf("%s", s);
-  printf("\nftell: %ld\n", ftell(f));
+  long int rand_num = (rand() % count);
+  printf("\nCOUNT: %d | RAND NUM: %ld\n", count,  rand_num);
+  fseek(f, rand_num, SEEK_SET);
   
-
+  fgets(word, 80, f);
+  fgets(word, 80, f);
+  printf("ftell: %ld\n", ftell(f));
+  printf("%s", word);
   
+  int word_len = sizeof(word)/sizeof(word[0]);
+  char *p = &word[word_len];
+    
   fclose(f);
   
   return 0;
